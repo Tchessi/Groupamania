f6f49fe30da476b4be5ed68210c6e2707a4c7a61
Tchessi Pre <tchesspreza@yahoo.fr>
2022-01-27 18:41:34 +0100

    Création Components, router, store,views,

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div
+      id="nav"
+      class="sticky-top justify-content-start justify-content-sm-center mb-3 p-3"
+    >
+      <router-link to="/">
+        <img
+          id="logo"
+          :src="require('./assets/icon-removebg-preview (1).png')"
+          alt="logo Groupamania"
+        />
+      </router-link>
+    </div>
+    <NotificationAlert />
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NotificationAlert from '../src/components/NotificationAlert';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    NotificationAlert,
+  },
+};
 </script>
 
 <style lang="scss">
@@ -22,7 +33,30 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+#nav {
+  background: #17a2b8;
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  margin: 15px;
+  #logo {
+    max-width: 300px;
+    height: 5.5em;
+  }
+}
+@media screen and (min-width: 280px) and (max-width: 767px) {
+  body,
+  .btn,
+  .form-control {
+    font-size: 13px !important;
+  }
+  #nav #logo {
+    max-width: 180px;
+  }
+}
+@media screen and (min-width: 767px) and (max-width: 991px) {
+  #nav #logo {
+    max-width: 200px;
+  }
 }
 </style>
---
+++ front-end/src/components/NotificationAlert.vue
@@ -0,0 +1,27 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+  name: 'NotificationAlert',
+  watch: {
+    message(newMessage) {
+      if (!newMessage) return;
+      this.$bvToast.toast(newMessage, {
+        title: 'Notification',
+        autoHideDelay: 4000,
+      });
+    },
+  },
+  computed: {
+    ...mapState(['notificationAlert']),
+    message() {
+      return this.notificationAlert.message;
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
